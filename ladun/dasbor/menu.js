4f88ab04f19896f40146167571efb7b9e1b9163b
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-06-20 21:58:14 +0700

    add

--- ladun/dasbor/menu.js
+++ ladun/dasbor/menu.js
@@ -1,1 +1,10 @@
 $('#tblMenu').DataTable();
+
+// Notification.requestPermission(permission => {
+//     if(permission === 'granted'){
+//         const myNotif = new Notification('Selamat ulang tahun', {
+//             body : 'Ini notifikasi'
+//         });
+//     }
+// });
+
