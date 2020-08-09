ff021090d164785d54dadb5b7bd303499581eec9
haxorsprogramming <haxorsuinsu@gmail.com>
2020-08-09 12:56:06 +0700

    update

--- engine/bind/dasbor/tentangAplikasi/tentangAplikasi.bind.php
+++ engine/bind/dasbor/tentangAplikasi/tentangAplikasi.bind.php
@@ -6,7 +6,7 @@
             <div class="card-body">
             <ul class="list-unstyled list-unstyled-border">
                     <li class="media" v-for='tp in timPengembang'>
-                      <img :src="'<?=STYLEBASE; ?>/dasbor/img/devTeam/'+tp.pic" class="mr-3 rounded-circle" width="50" src="" :alt="tp.nama">
+                      <img src="https://avatars0.githubusercontent.com/u/66120966?s=460&u=8392cc4234f8a24b5416d13ce160ac0536293983&v=4" class="mr-3 rounded-circle" width="50" src="" :alt="tp.nama">
                       <div class="media-body">
                         <div class="float-right text-primary">{{tp.pos}}</div>
                         <div class="media-title">{{tp.nama}}</div>
@@ -22,7 +22,9 @@
         <div class="card card-primary">
             <div class="card-header"><h4>Daftar Kontributor</h4></div>
             <div class="card-body">
+                <p id='capContributors'>
 
+                </p>
             </div>
         </div>
     </div>
--- ladun/dasbor/tentangAplikasi.js
+++ ladun/dasbor/tentangAplikasi.js
@@ -2,9 +2,26 @@ var divTentangAplikasi = new Vue({
     el : '#divTentangAplikasi',
     data : {
         timPengembang : [
-            {nama : 'Hasnah Nur Ardita', pos : 'Project Manager & Front End', pic : 'hasnah_ardita.jpg', email : 'alditha.forum@gmail.com'},
+            {nama : 'Hasnah Nur Ardita', pos : 'Project Manager & Front End', pic : 'http://localhost/Nadha-Resto/dasbor/img/devTeam/hasnah_ardita.jpg', email : 'alditha.forum@gmail.com'},
             {nama : 'Aditia Darma Nst', pos : 'Backend Dev', pic : 'aditia_darma.jpg', email : 'siti.okthary@gmail.com'},
             {nama : 'Adam Falizufa Sagara', pos : 'Microservice', pic : 'adam_flz.png', email : 'adamflz@gmail.com'}
         ]
     } 
 });
+
+var listKontributor = '';
+
+$.post('http://api.haxors.or.id/haxors-product/contributors/getContributors.php', function(data){
+    let obj = JSON.parse(data);
+    console.log(obj);
+    obj.forEach(renderList);
+
+    function renderList(item, index){
+        listKontributor = listKontributor + obj[index].nama + " - ";
+    }
+
+});
+
+setTimeout(function(){
+    $('#capContributors').html(listKontributor);
+}, 500);
