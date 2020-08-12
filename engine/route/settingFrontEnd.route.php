2b747683d2e5f9c3a2da5da4de73a8729d9a1f61
haxorsprogramming <haxorsuinsu@gmail.com>
2020-08-13 01:42:39 +0700

    update

---
+++ engine/route/settingFrontEnd.route.php
@@ -0,0 +1,13 @@
+<?php
+
+class settingFrontEnd extends Route{
+
+    private $sn = 'settingFrontEndData';
+    private $su = 'utilityData';
+
+    public function index()
+    {
+        echo "Setting front end!!";
+    }
+
+}
